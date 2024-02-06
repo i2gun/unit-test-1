02aa221fdc6a405e1ba6810432e8aa847e93127d
i2gun <i2gun@bk.ru>
2024-02-06 22:43:16 +0600

    initial commit

---
+++ src/seminars/first/Calculator/Calculator.java
@@ -0,0 +1,36 @@
+package seminars.first.Calculator;
+
+public class Calculator {
+    public static int calculation(int firstOperand, int secondOperand, char operator) {
+        int result;
+
+        switch (operator) {
+            case '+':
+                result = firstOperand + secondOperand;
+                break;
+            case '-':
+                result = firstOperand - secondOperand;
+                break;
+            case '*':
+                result = firstOperand * secondOperand;
+                break;
+            case '/':
+                if (secondOperand != 0) {
+                    result = firstOperand / secondOperand;
+                    break;
+                } else {
+                    throw new ArithmeticException("Division by zero is not possible");
+                }
+            default:
+                throw new IllegalStateException("Unexpected value operator: " + operator);
+        }
+        return result;
+    }
+
+    public static double calculateDiscount(int price, int discount) {
+        if (price < 0 || discount < 0) throw new ArithmeticException();
+
+        return price * (1 - (double) discount / 100);
+    }
+
+}
---
+++ src/seminars/first/Calculator/CalculatorTest.java
@@ -0,0 +1,46 @@
+package seminars.first.Calculator;
+
+import seminars.first.Calculator.Calculator;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculatorTest {
+    public static void main(String[] args) {
+
+        // Проверка дополнительного функционала calculateDiscount
+        assertThat(Calculator.calculateDiscount(200, 60)).isEqualTo(80.0);
+        assertThat(Calculator.calculateDiscount(20, 20)).isEqualTo(16);
+        assertThat(Calculator.calculateDiscount(120, 50)).isEqualTo(60);
+        assertThat(Calculator.calculateDiscount(100, 50)).isEqualTo(50);
+
+        // Проверка ожидаемых исключений, с использованием утверждений AssertJ:
+        assertThatThrownBy(() ->
+                Calculator.calculateDiscount(-200, 40)
+        ).isInstanceOf(ArithmeticException.class);
+
+        assertThatThrownBy(() ->
+                Calculator.calculateDiscount(220, -40)
+        ).isInstanceOf(ArithmeticException.class);
+
+        assertThatThrownBy(() ->
+                Calculator.calculateDiscount(-220, -40)
+        ).isInstanceOf(ArithmeticException.class);
+
+
+
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+//        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+//        assertThat(Calculator.calculation(2, 2, '-')).isEqualTo(0);
+//        assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
+//        assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
+
+        // Проверка ожидаемого исключения, с использованием утверждений AssertJ:
+//        assertThatThrownBy(() ->
+//                Calculator.calculation(8, 4, '_')
+//        ).isInstanceOf(IllegalStateException.class);
+//
+//        System.out.println(Calculator.calculation(2_147_483_647, 1, '+')); // integer overflow
+//        System.out.println(Calculator.squareRootExtraction(169));
+
+    }
+}
